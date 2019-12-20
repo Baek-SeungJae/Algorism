f8fd89a243749c8dcc156dd2b92471a62a909640
Baek-SeungJae <tmdwo897@naver.com>
2019-12-20 20:34:45 +0900

    12/20 프로그래머스

---
+++ programmers/Programmers_12912.java
@@ -0,0 +1,29 @@
+package programmers;
+
+public class Programmers_12912 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 3;
+		solution(a, b);
+
+	}
+
+	public static long solution(int a, int b) {
+		long answer = 0;
+		
+		int s;
+		int e;
+		if (a >= b) {
+			s = b;
+			e = a;
+		} else {
+			s = a;
+			e = b;
+		}
+		for(int i = s; i<=e; i++) {
+			answer += i;
+		}
+		System.out.println(answer);
+		return answer;
+	}
+}
