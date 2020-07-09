ae5900d866296387d35d83e4621bb68d92cbb1b4
Baek-SeungJae <tmdwo897@naver.com>
2020-07-09 22:02:21 +0900

    7/9 알고리즘, 늦은 매일프로그래밍

---
+++ daily/O20200628.java
@@ -0,0 +1,28 @@
+package dally;
+
+public class O20200628 {
+
+	public static void main(String[] args) {
+		int[] input = new int[] {-40, -30, -20, 2};
+		System.out.println(solution(input));
+	}
+	public static int solution(int[] input) {
+		int index = input.length/2;
+		while(true) {
+			if(input[index]==index) {
+				return index;
+			} else if(input[index]>index) {
+				index = index/2;
+				if(index == index/2) {
+					return -1;
+				}
+			} else {
+				index = (input.length+index)/2;
+				if(index == (input.length+index)/2) {
+					return -1;
+				}
+			}
+		}
+	}
+
+}
---
+++ daily/X20200621.java
@@ -0,0 +1,9 @@
+package daily;
+
+public class X20200621 {
+
+	public static void main(String[] args) {
+		
+	
+	}
+}
