ef805d11ab810e4b2f48752a955e0a199000e7e0
Baek-SeungJae <tmdwo897@naver.com>
2020-07-30 13:27:33 +0900

    7/30 프로그래머스

---
+++ programmers/programmers_62048.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class programmers_62048 {
+
+	public static void main(String[] args) {
+		int w = 8;
+		int h = 12;
+		System.out.println(solution(w, h));
+
+	}
+
+	public static long solution(int w, int h) {
+		long answer = 0;
+		double slope = (double) h / w;
+		for (int i = 0; i < h; i++) {
+			answer += ((int) (i / slope));
+		}
+		answer *= 2;
+		return answer;
+	}
+}
